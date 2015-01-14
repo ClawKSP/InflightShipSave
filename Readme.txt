d71e7a34fe955cbc1686402394921ffae294ab6b
ClawKSP <ClawKSP@gmail.com>
2015-01-13 22:15:12 -0500

    v0.1.0
    Initial Release. Go to your ship in-flight and press F6 to save. It will
    create a .craft in the VAB directory with _Rescued appended to the
    ship's name.

---
+++ Readme.txt
@@ -0,0 +1,63 @@
+InflightShipSave
+
+=========================
+
+
+Saves a ship that is in-flight, in case you launched it and forgot to save.
+
+When you want to save a ship, visit it in-flight and press F6. This will save the ship to a .craft file in your current game and will have the name "_Rescued" appended. 
+
+===>  saves\[CurrentSaveName]\Ships\VAB\[NameOfShip]_Rescued.craft
+
+
+NOTE: Still a work in progress. There are certain attributes about ships that, when active, cause the editor to throw NullRefs. Please let me know if you run into this, and provide a save and/or the offending .craft file.
+
+
+Installation
+
+============
+
+
+Just place the InflightShipSave.dll in your GameData directory.
+
+
+
+
+License
+
+=======
+
+
+
+Covered under the CC-BY-NC-SA license. See the license.txt for more details.
+
+(https://creativecommons.org/licenses/by-nc-sa/4.0/)
+
+
+
+
+
+Change Log
+
+==========
+v0.1.7d (6 Jan 15) - Rebuild of SymmetryActionFix.dll. More robust at handling nested symmetry and collecting symmetric parts / action groups.
+v0.1.7c (4 Jan 15) - Regressed SymmetryActionFix.dll again. Found further bugs that break new KSP symmetry features.
+v0.1.7b (29 Dec 14) - Reinstated SymmetryActionFix.dll for KSP v0.90.0.705.
+v0.1.7a (29 Dec 14) - Updated CrewRosterFreezeFix.dll to be a bit more robust. Reduced StickyLaunchPadFix.dll log spam.
+v0.1.7 (23 Dec 14) - Initial release StickyLaunchPadFix.dll.
+v0.1.6 (20 Dec 14) - Initial release CrewRosterFreezeFix.dll, regressed SymmetryActionFix.dll (broken in KSP v0.90), depricated LargeCraftLaunchFix.dll
+v0.1.5c (10 Dec 14) - AnchoredDecouplerFix.dll updated for compatilibity with Kerbquake.
+v0.1.5b (23 Nov 14) - EVAEjectionFix now nullifies ladder slide bug for initial EVA, and added minor error checking for KerbalDebrsFix.
+v0.1.5a (22 Nov 14) - KerbalDebrisFix now properly recovers names for already frozen kerbals.
+v0.1.5 (22 Nov 14) - Initial release of KerbalDebrisFix.dll
+v0.1.4d (22 Nov 14) - LargeCraftLaunchFix.dll now functions correctly with Kerbal Joint Reinforcement installed.
+v0.1.4c (6 Nov 14) - SymmetryActionFix.dll now handles action groups buried in symmetry within symmetry.
+v0.1.4b (5 Nov 14) - Added some error checking to SymmetryActionFix.dll
+v0.1.4a (3 Nov 14) - Updated LargeCraftLaunchFix.dll to be a bit more robust. Now works through quickloads, scene transitions, and coming within physics range.
+v0.1.4 (2 Nov 14) - Initial release of LargeCraftLaunchFix.dll
+v0.1.3 & v0.1.3a (1 Nov 14) - Initial release of SymmetryActionFix.dll
+v0.1.2a (27 Oct 14) - Removed "Reset" message from ChuteQuickloadFixer.dll and recompiled for .NET 3.5, updated readme to accomodate releases
+v0.1.2 (21 Oct 14) - Reworked AnchoredDecouplerFix to better handle struts and prevent decouplers from ripping off. (Should work like pre KSP v0.24.2)
+v0.1.1a (21 Oct 14) - Updated error handling in EVAEjectionFix to prevent log spam and kerbal lockup with incompatible mod
+v0.1.1 (19 Oct 14) - Release of EVAEjectionFix.dll and reduced some log spam with ChuteQuickloadFixer and AnchoredDecouplerFix
+v0.1 (17 Oct 14) - Initial release, includes ChuteQuickloadFixer and AnchoredDecouplerFix
---
+++ source/InflightShipSave.cs
@@ -0,0 +1,114 @@
+﻿/*
+ * This module written by Claw. For more details please visit
+ * http://forum.kerbalspaceprogram.com/threads/97285-0-25-Stock-Bug-Fix-Modules 
+ * 
+ * This mod is covered under the CC-BY-NC-SA license. See the license.txt for more details.
+ * (https://creativecommons.org/licenses/by-nc-sa/4.0/)
+ * 
+ * Written for KSP v0.90.0
+ *
+ * InflightShipSave v0.1.0
+ * 
+ * This plugin allows the user to save the active, in-flight vessel back out to a .craft file.
+ * 
+ * Things to fix:
+ * - Make the save key bindable (so that it can be changed in a .cfg file)
+ * - Adjust the rotation/position of the root part, so that the craft isn't sideways in the ground.
+ * 
+ * 
+ * Change Log:
+ * 
+ * v0.1.0 (13 Jan 15) - Initial release
+ * 
+ */
+
+using UnityEngine;
+using KSP;
+
+namespace ClawKSP
+{
+    [KSPAddon(KSPAddon.Startup.Flight, false)]
+    public class InflightShipSave : MonoBehaviour
+    {
+        //private string Binding;
+
+        public void Start ()
+        {
+            Debug.Log("InflightShipSave.Start()");
+
+            //ConfigNode CNBinding = new ConfigNode("Game");
+            //CNBinding = GameDatabase.Instance.GetConfigNode("INFLIGHT_SHIP_SAVE");
+
+            //Binding = CNBinding.GetValue("primary");
+            //Binding = (GameDatabase.Instance.GetConfigNode("INFLIGHT_SHIP_SAVE")).GetValue("primary");
+        }
+
+        public void Update ()
+        {
+            // Debug.LogWarning("InflightShipSave.Update()");
+
+            if (Input.GetKeyDown(KeyCode.F6))
+            {
+                Vessel VesselToSave = FlightGlobals.ActiveVessel;
+                string ShipName = VesselToSave.vesselName;
+                Debug.LogWarning("Saving: " + ShipName);
+
+                ShipConstruct ConstructToSave = new ShipConstruct(ShipName + "_Rescued", "TempDescription", VesselToSave.parts[0]);
+
+                Quaternion OriginalRotation = VesselToSave.vesselTransform.rotation;
+                Vector3 OriginalPosition = VesselToSave.vesselTransform.position;
+
+                VesselToSave.SetRotation(new Quaternion(0, 0, 0, 1));
+                Vector3 ShipSize = ShipConstruction.CalculateCraftSize(ConstructToSave);
+                VesselToSave.SetPosition(new Vector3(0, ShipSize.y+2, 0));
+
+                ConfigNode CN = new ConfigNode("ShipConstruct");
+                CN = ConstructToSave.SaveShip();
+                CleanNodes(CN);
+
+                VesselToSave.SetRotation(OriginalRotation);
+                VesselToSave.SetPosition(OriginalPosition);
+
+                Debug.LogWarning("Facility: " + ConstructToSave.shipFacility);
+
+                if (ConstructToSave.shipFacility == EditorFacility.SPH)
+                {
+                    Debug.LogWarning("Ship Saved: " + UrlDir.ApplicationRootPath + "saves/" + HighLogic.SaveFolder
+                        + "/Ships/SPH/" + ShipName + "_Rescued.craft");
+                    CN.Save(UrlDir.ApplicationRootPath + "saves/" + HighLogic.SaveFolder
+                        + "/Ships/SPH/" + ShipName + "_Rescued.craft");
+                }
+                else
+                {
+                    Debug.LogWarning("Ship Saved: " + UrlDir.ApplicationRootPath + "saves/" + HighLogic.SaveFolder
+                        + "/Ships/VAB/" + ShipName + "_Rescued.craft");
+                    CN.Save(UrlDir.ApplicationRootPath + "saves/" + HighLogic.SaveFolder
+                        + "/Ships/VAB/" + ShipName + "_Rescued.craft");
+                }
+            }
+            
+        }
+
+        private void CleanNodes (ConfigNode CN)
+        {
+            if (null == CN) { return; }
+
+            CN.SetValue("EngineIgnited", "False");
+            CN.SetValue("currentThrottle", "0");
+            CN.SetValue("Staged", "False");
+            CN.SetValue("sensorActive", "False");
+            CN.SetValue("active", "False");
+            CN.SetValue("throttle", "0");
+            CN.SetValue("generatorIsActive", "False");
+
+            for(int IndexNodes = 0; IndexNodes < CN.nodes.Count; IndexNodes++)
+            {
+                CleanNodes(CN.nodes[IndexNodes]);
+            }
+        }
+        public void OnDestroy ()
+        {
+            Debug.Log("InflightShipSave.OnDestroy()");
+        }
+    }
+}
